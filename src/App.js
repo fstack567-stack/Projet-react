348d465147a506d6a24544ff3e801612600bfb4f
Safouane Rady <safouanekira2001@gmail.com>
2025-12-20 21:29:22 +0100

    fixe

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
         <Route path="/stagiaires/ajermoune" element={<Asma />} />
         <Route path="/stagiaires/abbou" element={<Meriem />} />
         <Route path="/stagiaires/Menguit" element={<Soukaina />} />
+        <Route path="/stagiaires/rady" element={<Safouane />} />
         <Route path="/stagiaires/Aidane" element={<Soufyane />} />
         <Route path="/stagiaires/bouhssine" element={<Hajar />} />
         <Route path="/stagiaires/sina" element={<Sina />} />
--- src/container.js
+++ src/container.js
@@ -4,6 +4,7 @@ const users = [
   { id: "ajermoune", nom: "Ajermoune", prenom: "Asma" },
   { id: "abbou", nom: "Abbou", prenom: "Meriem" },
   { id: "Aidane", nom: "Aidane", prenom: "Soufyane" },
+  { id: "rady", nom: "Rady", prenom:"Safouane" },
   { id: "Menguit", nom: "Menguit", prenom: "Soukaina" },
   { id: "bouhssine", nom: "Bouhssine", prenom: "Hajar" },
   { id: "sina", nom: "Sina", prenom: "Youssef" },

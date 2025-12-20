d1c0f9d7d0540c59c03d65e79d4d6fdf0f7495d2
Youness Ailal <younessaylal21@gmail.com>
2025-12-20 22:01:27 +0100

    fixed

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import Asma from "./components/asma";
 import Hajar from "./components/hajar";
 import Meriem from "./components/meriem";
 import Soukaina from "./components/soukaina";
-<<<<<<< HEAD
 import Safouane from "./components/safouane";
 import Sina from "./components/sina";
 import Ailal from "./components/Ailal";
 import Soufyane from "./components/soufyane";
-=======
-import Younes from './components/younes';
+import Younes from './components/elkaidi';
 
 
->>>>>>> feature-add-component
 
 function App() {
   return (
@@ -26,13 +23,11 @@ function App() {
         <Route path="/stagiaires/ajermoune" element={<Asma />} />
         <Route path="/stagiaires/abbou" element={<Meriem />} />
         <Route path="/stagiaires/Menguit" element={<Soukaina />} />
-<<<<<<< HEAD
         <Route path="/stagiaires/rady" element={<Safouane />} />
         <Route path="/stagiaires/Aidane" element={<Soufyane />} />
         <Route path="/stagiaires/bouhssine" element={<Hajar />} />
         <Route path="/stagiaires/sina" element={<Sina />} />
         <Route path="/stagiaires/ailal" element={<Ailal />} />
-=======
 
 
         <Route path='/stagiaires/bouhssine' element={<Hajar/>} />
@@ -40,7 +35,6 @@ function App() {
 
 
 
->>>>>>> feature-add-component
       </Routes>
     </BrowserRouter>
   );

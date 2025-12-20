f461d6d13da41d57517d8b041be7806ede1579de
Youness Ailal <younessaylal21@gmail.com>
2025-12-20 21:41:15 +0100

    fixed

--- src/components/asma.js
+++ src/components/asma.js
@@ -3,17 +3,29 @@ import "../styles/asma.css";
 
 export default function Asma() {
   return (
-    <div className="container">
+    <div className="container-asma">
       <img src="/images/asma.jpg" alt="Asma" className="profile-asma" />
 
       <div className="card-asma">
-        <p><b>Nom :</b> Ajermoune</p>
-        <p><b>Prénom :</b> Asma</p>
-        <p><b>Date de naissance :</b> 05/03/2004</p>
-        <p><b>Email :</b> ajermouneasma@gmail.com</p>
-        <p><b>Adresse :</b> Agadir, Maroc</p>
+        <p>
+          <b>Nom :</b> Ajermoune
+        </p>
+        <p>
+          <b>Prénom :</b> Asma
+        </p>
+        <p>
+          <b>Date de naissance :</b> 05/03/2004
+        </p>
+        <p>
+          <b>Email :</b> ajermouneasma@gmail.com
+        </p>
+        <p>
+          <b>Adresse :</b> Agadir, Maroc
+        </p>
 
-        <Link to="/" className="btn-retour-a">Retour</Link>
+        <Link to="/" className="btn-retour-a">
+          Retour
+        </Link>
       </div>
     </div>
   );

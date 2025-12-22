1f87f258f9131cce1c3182d2e386c58d4b36c31b
ElorfSara <elorfsara27@gmail.com>
2025-12-22 16:03:13 +0100

    Update sara.js

--- src/components/sara.js
+++ src/components/sara.js
@@ -1,19 +1,57 @@
 import { Link } from "react-router-dom";
+import "../styles/style.css";
 
- export default function SaraE() {
+import calendarIcon from "../assets/icons/calendar.png";
+import emailIcon from "../assets/icons/email.png";
+import locationIcon from "../assets/icons/location.png";
+import cefIcon from "../assets/icons/cef.png"
+
+export default function Sara() {
   return (
-   <div className="profile-card">
+    <div className="container-alanbar">
+      <div className="profile-card-alanbar">
 
-   <img className="profile-img" src="/images/sara.jpg" />
-  <div className="profile-details">
-    <h2 className="name">Sara</h2> 
-    <h3 className="lastname">El Orf</h3>
-    <p className="dob" style={{ color: 'pink' }}> <span>🎂</span> 8/05/2006</p> <br></br>
-    <p className="email"><span>📧</span> elorfsara27@gmail.com</p> 
-     
-     <Link to="/" className="btn-retour">Retour</Link>
-  </div>
-</div>
+       
+        <div className="image-wrapper-alanbar">
+          <img
+            src="/images/sara.jpg"
+            alt="Sara"
+            className="profile-alanbar"
+          />
+        </div>
 
+        <div className="card-alanbar">
+          <h2 className="name-alanbar">El Orf Sara</h2>
+          <p className="subtitle-alanbar">Web Development Student</p>
+
+          <div className="info-alanbar">
+            <p>
+              <img src={calendarIcon} alt="Date" className="info-icon" />
+              08/ 05 / 2006
+            </p>
+
+            <p>
+              <img src={emailIcon} alt="Email" className="info-icon" />
+              2006050800191@ofppt-edu.ma
+            </p>
+
+            <p>
+              <img src={locationIcon} alt="Location" className="info-icon" />
+              Agadir, Morocco
+            </p>
+            <p>
+              <img src={cefIcon} alt="cef" className="info-icon" />
+              2006050800191
+            </p>
+
+          </div>
+
+          <Link to="/" className="btn-retour-md">
+            ⬅ Retour
+          </Link>
+        </div>
+
+      </div>
+    </div>
   );
 }

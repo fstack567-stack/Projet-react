b6be6bd98dab97002e3c5d526efeca15bc0e7483
SafouaneRady <safouanekira2001@gmail.com>
2025-12-22 17:05:42 +0100

    Update safouane.js

--- src/components/safouane.js
+++ src/components/safouane.js
@@ -1,27 +1,56 @@
 import { Link } from "react-router-dom";
+import "../styles/style.css";
 
+import calendarIcon from "../assets/icons/calendar.png";
+import emailIcon from "../assets/icons/email.png";
+import locationIcon from "../assets/icons/location.png";
+import cefIcon from "../assets/icons/cef.png"
 
 export default function Safouane() {
   return (
-    <div className="page">
-      <div className="card">
-        <img src="/images/safouane.jpg" alt="Profile" className="profile-img" />
+    <div className="container-alanbar">
+      <div className="profile-card-alanbar">
 
-        <h1>Safouane Rady</h1>
-        <h2>Full Stack Web Developer</h2>
-
-        <div className="info">
-          <p>
-            <span>Email</span> safouanekira2001@gmail.com
-          </p>
-          <p>
-            <span>Birthday</span> 14/05/2001
-          </p>
-          <p>
-            <span>CEF</span> 2001051400454
-          </p>
-          <Link to="/" className="btn-retour">Retour</Link>
+       
+        <div className="image-wrapper-alanbar">
+          <img
+            src="/images/safouane.jpg"
+            alt="Safouane"
+            className="profile-alanbar"
+          />
         </div>
+
+        <div className="card-alanbar">
+          <h2 className="name-alanbar">Safouane Rady</h2>
+          <p className="subtitle-alanbar">Web Development Student</p>
+
+          <div className="info-alanbar">
+            <p>
+              <img src={calendarIcon} alt="Date" className="info-icon" />
+              14 / 05 / 2001
+            </p>
+
+            <p>
+              <img src={emailIcon} alt="Email" className="info-icon" />
+              2001051400454@ofppt-edu.ma
+            </p>
+
+            <p>
+              <img src={locationIcon} alt="Location" className="info-icon" />
+              Agadir, Morocco
+            </p>
+            <p>
+              <img src={cefIcon} alt="cef" className="info-icon" />
+              2001051400454
+            </p>
+
+          </div>
+
+          <Link to="/" className="btn-retour-md">
+            ⬅ Homepage
+          </Link>
+        </div>
+
       </div>
     </div>
   );

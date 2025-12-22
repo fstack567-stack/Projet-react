2dce9e417fa35679a7475facc358a22a7ea699c6
asma <ajermouneasma@gmail.com>
2025-12-22 15:46:45 +0100

    modification de asma

--- src/components/asma.js
+++ src/components/asma.js
@@ -1,31 +1,56 @@
 import { Link } from "react-router-dom";
+import "../styles/style.css";
 
+import calendarIcon from "../assets/icons/calendar.png";
+import emailIcon from "../assets/icons/email.png";
+import locationIcon from "../assets/icons/location.png";
+import cefIcon from "../assets/icons/cef.png"
 
-export default function Asma() {
+export default function Alanbar() {
   return (
-    <div className="container-asma">
-      <img src="/images/asma.jpg" alt="Asma" className="profile-asma" />
+    <div className="container-alanbar">
+      <div className="profile-card-alanbar">
 
-      <div className="card-asma">
-        <p>
-          <b>Nom :</b> Ajermoune
-        </p>
-        <p>
-          <b>Prénom :</b> Asma
-        </p>
-        <p>
-          <b>Date de naissance :</b> 05/03/2004
-        </p>
-        <p>
-          <b>Email :</b> ajermouneasma@gmail.com
-        </p>
-        <p>
-          <b>Adresse :</b> Agadir, Maroc
-        </p>
+       
+        <div className="image-wrapper-alanbar">
+          <img
+            src="/images/asma.jpg"
+            alt="Asma Ajermoune"
+            className="profile-alanbar"
+          />
+        </div>
 
-        <Link to="/" className="btn-retour-asma">
-          Retour
-        </Link>
+        <div className="card-alanbar">
+          <h2 className="name-alanbar">Asma Ajermoune</h2>
+          <p className="subtitle-alanbar">Web Development Student</p>
+
+          <div className="info-alanbar">
+            <p>
+              <img src={calendarIcon} alt="Date" className="info-icon" />
+              25 / 04 / 2001
+            </p>
+
+            <p>
+              <img src={emailIcon} alt="Email" className="info-icon" />
+              2004030500394@ofppt-edu.ma
+            </p>
+
+            <p>
+              <img src={locationIcon} alt="Location" className="info-icon" />
+              Agadir, Morocco
+            </p>
+            <p>
+              <img src={cefIcon} alt="cef" className="info-icon" />
+              2004030500394
+            </p>
+
+          </div>
+
+          <Link to="/" className="btn-retour-md">
+            ⬅ Retour
+          </Link>
+        </div>
+
       </div>
     </div>
   );
